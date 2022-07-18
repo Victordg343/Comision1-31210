320013f0896fdcdaab9de219d59b0b202185c3d0
Victordg343 <victordg.gonzalez@gmail.com>
2022-07-18 10:20:43 -0300

    agrege una barra navegadora

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navbar = () => {
+return (
+    <div>
+
+<button className="NavBar__boton">Menú</button>
+<button className="NavBar__boton">Productos</button>
+<button className="NavBar__boton">Sobre Nosotros</button>
+<button className="NavBar__boton">Galeria de imagenes</button>
+<button className="NavBar__boton">Preguntas frecuentes</button>
+
+
+    </div>
+)
+}
+
+export default navbar
